21ee4f95b3f9457947cc5ce52f60ffe252676209
Michał Nogas <nogasmichal@gmail.com>
2018-05-04 11:55:13 +0200

    Initial commit

---
+++ src/Providers/PlentyBackendServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PlentyBackend\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class PlentyBackendServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
